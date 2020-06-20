70b481724dad0369d75982ab129898186bdf8f44
Ian <ian@libelnet.nl>
2020-06-20 23:53:05 +0200

    Fixed Wanguard API

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name="SystemecWanGuardAPI",
     version="1.0",
     description="WanGuard API integration for Systemec",
-    url="https://github.com/ianket/ElasticsearchAPI",
+    url="https://github.com/ianket/WanGuardAPI",
     packages=setuptools.find_packages(),
     classifiers=[
         'Programming Language :: Python',
